a7fd95d6e18b3107067e23e719dfa275d5592cdd
sorhawell <sorhawell@gmail.com>
2022-11-03 09:41:34 +0100

    add grid interpolation example

--- R/expr__expr.R
+++ R/expr__expr.R
@@ -2300,5 +2300,20 @@ Expr_inspect = function(fmt = "{}") {
 #' @return Expr
 #' @aliases interpolate
 #' @examples
-#' pl$empty_select(pl$lit(c(1,NA,4,NA,100))$interpolate())
+#' pl$empty_select(pl$lit(c(1,NA,4,NA,100,NaN,150))$interpolate())
+#'
+#' #x, y interpolation over a grid
+#' df_original_grid = pl$DataFrame(list(
+#'   grid_points = c(1, 3, 10),
+#'   values = c(2.0, 6.0, 20.0)
+#' ))
+#' df_new_grid = pl$DataFrame(list(grid_points = (1:10)*1.0))
+#'
+#' # Interpolate from this to the new grid
+#' df_new_grid$join(
+#'   df_original_grid, on="grid_points", how="left"
+#' )$with_columns(pl$col("values")$interpolate())
 Expr_interpolate = "use_extendr_wrapper"
+
+
+
