feeb4092c1f3368e266a621016035500d58bdb22
sorhawell <sorhawell@gmail.com>
2022-11-04 02:13:29 +0100

    reinclude devtools in renv.lock

--- misc/hello_renv.R
+++ misc/hello_renv.R
@@ -1,5 +1,5 @@
 ## only used for renv to discover packages which should also be added to lock
 
 ## add any package below, the sxript will never be actually run
-#library(devtools)
+library(devtools)
 #library(pkgdown)
