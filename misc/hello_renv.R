a8c18ded50f62952b565d59c6ef637c4f921242b
sorhawell <sorhawell@gmail.com>
2022-11-04 09:26:06 +0100

    explicit renv dependencies via description

--- misc/hello_renv.R
+++
@@ -1,5 +0,0 @@
-## only used for renv to discover packages which should also be added to lock
-
-## add any package below, the sxript will never be actually run
-library(devtools)
-#library(pkgdown)
