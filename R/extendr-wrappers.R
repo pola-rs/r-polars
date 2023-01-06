363f5a48f9299d0451a3156b989e2a6d08372426
Dewey Dunnington <dewey@fishandwhistle.net>
2023-01-06 14:19:26 -0400

    update wrapper

--- R/extendr-wrappers.R
+++ R/extendr-wrappers.R
@@ -72,7 +72,7 @@ DataFrame$to_struct <- function(name) .Call(wrap__DataFrame__to_struct, self, na
 
 DataFrame$unnest <- function(names) .Call(wrap__DataFrame__unnest, self, names)
 
-DataFrame$export_stream <- function() invisible(.Call(wrap__DataFrame__export_stream, self))
+DataFrame$export_stream <- function(stream_ptr) invisible(.Call(wrap__DataFrame__export_stream, self, stream_ptr))
 
 #' @export
 `$.DataFrame` <- function (self, name) { func <- DataFrame[[name]]; environment(func) <- environment(); func }
