3038587d64d3abcf029c97ad52c4240e434a8509
sorhawell <sorhawell@gmail.com>
2023-01-13 21:19:53 +0100

    revise docs

--- R/pkg-nanoarrow.R
+++ R/pkg-nanoarrow.R
@@ -1,4 +1,3 @@
-
 #' rpolars to nanoarrow and arrow
 #' @description Conversion via native apache arrow array stream (fast), THIS REQUIRES ´nanoarrow´
 #' @name nanoarrow
@@ -6,30 +5,21 @@
 #' @param ... not used right now
 #' @param schema must stay at default value NULL
 #' @keywords nanoarrow_interface
-#' @return a nanoarrow array stream
-#' @usage as_nanoarrow_array_stream(x, ..., schema = NULL)
+#' @return - a nanoarrow array stream
 #' @details
 #' The following functions enable conversion to `nanoarrow` and `arrow`.
 #' Conversion kindly provided by "paleolimbot / Dewey Dunnington" Author of `nanoarrow`.
 #' Currently these conversions are the fastest way to convert from polars to R.
-#'
-#' These conversion function are not exported/public are only accessible via `nanoarrow`
-#' e.g. \cr\cr
-#' `df = pl$DataFrame(iris)`\cr
-#' `nanoarrow::nanoarrow_allocate_array_stream(df)`\cr\cr
-#' or\cr\cr
-#' `library(nanoarrow)`\cr
-#' `nanoarrow_allocate_array_stream(df)`\cr
 #'
 #' @aliases array_stream arrow nanoarrow record_batch_reader arrow_table
 #' read more at \link{https://github.com/apache/arrow-nanoarrow/r}
 #'
 #' @examples
 #' library(nanoarrow)
-#' library(rpolars)
-#' df = pl$DataFrame(iris)
+#' df = pl$DataFrame(mtcars)
 #' nanoarrow_array_stream = as_nanoarrow_array_stream(df)
 #' rdf = as.data.frame(nanoarrow_array_stream)
+#' print(head(rdf))
 as_nanoarrow_array_stream.DataFrame = function(x, ..., schema = NULL) {
   # Don't support the schema argument yet
   stopifnot(is.null(schema))
@@ -39,29 +29,30 @@ as_nanoarrow_array_stream.DataFrame = function(x, ..., schema = NULL) {
 }
 
 #' @rdname nanoarrow
-#' @return a nanoarrow array schema
-#' @usage infer_nanoarrow_schema(x, ...)
+#' @return - a nanoarrow array schema
 #' @examples
 #' nanoarrow_array_schema = infer_nanoarrow_schema(df)
+#' print(nanoarrow_array_schema)
 infer_nanoarrow_schema.DataFrame = function(x, ...) {
   as_nanoarrow_array_stream.DataFrame(x)$get_schema()
 }
 
 #' @rdname nanoarrow
-#' @return an arrow record batch reader
-#' @usage as_record_batch_reader(x, ..., schema = NULL)
+#' @return  - an arrow table
 #' @examples
-#' arrow_record_batch_reader = as_record_batch_reader(df)
-as_record_batch_reader.DataFrame = function(x, ..., schema = NULL) {
-  arrow::as_record_batch_reader(as_nanoarrow_array_stream.DataFrame(x, schema = schema))
-}
-
-#' @rdname nanoarrow
-#' @return an arrow table
-#' @usage as_arrow_table(x, ..., schema = NULL)
-#' @examples
+#' library(arrow)
 #' arrow_table = as_arrow_table(df)
+#' print(arrow_table)
 as_arrow_table.DataFrame = function(x, ...) {
   reader = as_record_batch_reader.DataFrame(x)
   reader$read_table()
+}
+
+#' @rdname nanoarrow
+#' @return - an arrow record batch reader
+#' @examples
+#' arrow_record_batch_reader = as_record_batch_reader(df) #requires arrow
+#' print(arrow_record_batch_reader)
+as_record_batch_reader.DataFrame = function(x, ..., schema = NULL) {
+  arrow::as_record_batch_reader(as_nanoarrow_array_stream.DataFrame(x, schema = schema))
 }
