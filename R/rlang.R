3b88cd6c0988b92a362f8cd47c254e07e1e3cca3
sorhawell <sorhawell@gmail.com>
2023-01-13 15:26:30 +0100

    move vctr + pl$timeunits

--- R/rlang.R
+++ R/rlang.R
@@ -1,9 +1,8 @@
-# all functions in here are directly copied or adapted to mimic rlang functions
+# all small functions in here are adapted/copied to mimic rlang functions
 # The purpose is to make rpolars have no install dependencies at all.
 
 #to reimplement list2(...) in rust would be great to allow trailing commas
 # no highlevel implemetaion seems stable e.g. http://r-de-jeu.blogspot.com/2013/03/r-and-last-comma.html
-
 
 
 `%||%` = function (x, y)  {
@@ -16,15 +15,6 @@
 
 is_bool = function (x) {
   is.logical(x) && length(x)==1 && !is.na(x)
-}
-
-#TODO if needed base is_integerish on ok-result from rust utils try_f64_into_i64
-# is_integerish = function(x) {
-#   is.numeric(x) && all(!is.na(x)) && isTRUE(all(as.integer(x)==x))
-# }
-
-is_scalar_integerish = function(x) {
-  is.numeric(x) && length(x)==1L && !is.na(x) && isTRUE(as.integer(x)==x)
 }
 
 is_string = function(x) {
