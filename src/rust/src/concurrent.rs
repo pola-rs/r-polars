75cf2103a0fcd8f8e79b3ad56de260c20631aeba
Søren Havelund Welling <sorhawell@gmail.com>
2023-03-01 23:19:13 +0100

    Update concurrent.rs (#56)

--- src/rust/src/concurrent.rs
+++ src/rust/src/concurrent.rs
@@ -15,37 +15,38 @@ use std::result::Result;
 // and mostly single threaded only R
 
 //handle_thread_r_request() is a special case of the concurrent_handler()
-//handle_thread_r_request passes 3 closures to concurrent_handler() + 1 closure to lazyframe
-//1: What to run (polars collect), 2: what wrapper function to use, 3: what handler to execute code
-//4: Expr.map or such, passes an R user defined function in a closure to the lazyframe. This closure describes
+//handle_thread_r_request passes 2 closures to concurrent_handler() + 1 closure to lazyframe
+//1: What to run (polars collect), 2: what handler to execute code
+//3: Expr.map or such, passes an R user defined function in a closure to the lazyframe. This closure describes
 //how to request R functions run on the mainthread with a ThreadCom object.
 
 pub fn handle_thread_r_requests(lazy_df: pl::LazyFrame) -> pl::PolarsResult<DataFrame> {
-    //Closure 1: start concurrent handler and get final result
+    
     let res_res_df = concurrent_handler(
-        //give a closure doing what want if we could ... to run LazyFrame.collect() safely
-        move |tc| {
-            //start collect
+        // Closure 1: start concurrent handler and get final result
+        // This what we want to do in the first place ... to run LazyFrame.collect() thread-safe
+        
+        //this closure gets spawned in a child of main thred, tc is a ThreadCom struct
+        move |tc| { 
+            
+            //start polars .collect, which it self can spawn many new threads
             let retval = lazy_df.collect();
             //collect done, we're all done know
 
-            //drop local and global ThreadCom clone, signals to R serving thread to shut down.
+            //drop local and global ThreadCom clone, signals to main/R-serving thread to shut down.
             ThreadCom::kill_global(&CONFIG);
             drop(tc);
 
             //taadaaa return value
             retval
         },
-        //closure 2:
-        //out of hot loop call this R code, just retrieve high-level function wrapper from package
-        //This wrapper Series_udf_handler only sends a valid series pointer
 
-        //closure 3
-        //how should the R serving mainthread handle a user function requst?
-        //spoiler alert:: run the function, return the answer.
+        //closure 2
+        //how should the R-serving mainthread handle a user function requst?
+        //synopsis: run the user-R-function, input/ouput is a Series
         |(probj, s): (ParRObj, pl::Series)| -> Result<pl::Series, Box<dyn std::error::Error>> {
-            //
-            //get user defined function
+            
+            //unpack user-R-function
             let f = probj.0.as_function().ok_or_else(|| {
                 extendr_api::error::Error::Other(format!(
                     "provided input is not an R function but a: {:?}",
@@ -53,15 +54,19 @@ pub fn handle_thread_r_requests(lazy_df: pl::LazyFrame) -> pl::PolarsResult<Data
                 ))
             })?;
 
-            //run call udf with Series as input, return Robj (likeliy as Series)
+            //call user-R-function with Series as input, return Robj (likeliy as Series)
             let rseries_robj = f.call(pairlist!(Series(s)))?;
 
             //return may not
-            let s = Series::any_robj_to_pl_series_result(&rseries_robj);
-            Ok(s?)
+            let s = Series::any_robj_to_pl_series_result(&rseries_robj)?;
+
+            Ok(s)
         },
+
+        //CONFIG is "global variable" where threads can request a new ThreadCom
         &CONFIG,
     );
+    //concurrent handling complete
 
     //bubble on concurrent errors
     let res_df = res_res_df.map_err(|err| {
