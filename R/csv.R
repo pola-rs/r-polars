11269b9258b67808ce3678211e622c89e0531217
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-04-13 21:49:45 +0000

    fix doc for arg `infer_schema_length ` (#109)

--- R/csv.R
+++ R/csv.R
@@ -40,8 +40,9 @@
 #' Unnamed char vector: A null value per column.
 #' Named char vector.  A mapping from (names)column to a null value string(values).
 #'
-#' @param infer_schema_length (NULL is disable) Maximum number of lines to read to infer schema. If set to 0, all columns will
-#' be read as pl.Utf8. If set to None, a full table scan will be done (slow).
+#' @param infer_schema_length Maximum number of rows to read to infer the column
+#'   types. If set to 0, all columns will be read as UTF-8. If `NULL`, a full
+#'   table scan will be done (slow).
 #' @param skip_rows_after_header bool Skip this number of rows when the header is parsed.
 #' @param encoding either "utf8" or "utf8-lossy". Lossy means that invalid utf8 values are replaced with "?" characters.
 #' @param row_count_name String(NULL is disable), name of a added row count column
