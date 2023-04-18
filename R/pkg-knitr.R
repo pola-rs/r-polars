ef8413fe1f7559bdc289cde9b67074f3ad1401f6
eitsupi <ts1s1andn@gmail.com>
2023-04-18 15:57:52 +0000

    fix: support gfm output

--- R/pkg-knitr.R
+++ R/pkg-knitr.R
@@ -8,14 +8,16 @@ knit_print.DataFrame = function(x, ...) {
     Sys.setenv(POLARS_FMT_TABLE_INLINE_COLUMN_DATA_TYPE = .env_inline_data_type)
   })
 
-
   if (.env_formatting %in% c("", "ASCII_MARKDOWN") && .env_inline_data_type %in% c("", "true", "1")) {
     Sys.setenv(POLARS_FMT_TABLE_FORMATTING = "ASCII_MARKDOWN")
     Sys.setenv(POLARS_FMT_TABLE_INLINE_COLUMN_DATA_TYPE = "1")
   }
 
-  print(x) |>
-    capture.output() |>
-    paste(collapse = "\n") |>
-    knitr::asis_output()
+  out <- capture.output(print(x))
+
+  # Needs to insert a blank line
+  out <- c(out[1], "", out[-1]) |>
+    paste(collapse = "\n")
+
+  knitr::asis_output(out)
 }
