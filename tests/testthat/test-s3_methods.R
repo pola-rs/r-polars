cea20d463037bf3435941f09d24c857aa4a2e5b2
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-04-18 16:29:24 -0400

    extra test

--- tests/testthat/test-s3_methods.R
+++ tests/testthat/test-s3_methods.R
@@ -96,10 +96,12 @@ test_that("unique", {
     x = as.numeric(c(1, 1:5)),
     y = as.numeric(c(1, 1:5)),
     z = as.numeric(c(1, 1, 1:4)))
+  expect_equal(unique(df)$height, 5)
   expect_equal(unique(df, subset = "z")$height, 4)
   df = pl$DataFrame(
     x = as.numeric(c(1, 1:5)),
     y = as.numeric(c(1, 1:5)),
     z = as.numeric(c(1, 1, 1:4)))$lazy()
+  expect_equal(unique(df)$collect()$height, 5)
   expect_equal(unique(df, subset = "z")$collect()$height, 4)
 })
