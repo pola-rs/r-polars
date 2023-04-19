7f8d4ce90176e117845bd5d7260584d8354f3be2
eitsupi <ts1s1andn@gmail.com>
2023-04-19 13:26:24 +0000

    feat: df_print for polars DataFrame

--- R/pkg-knitr.R
+++ R/pkg-knitr.R
@@ -5,24 +5,44 @@
 #' @keywords DataFrame
 #' @export
 knit_print.DataFrame = function(x, ...) {
+  if (identical(knitr::opts_knit$get("rmarkdown.df_print"), "kable") || (getOption("polars.df_print", "default") == "kable")) {
+    kable_print_pl_df(x)
+  } else {
+    print(x)
+  }
+}
+
+#' @param x polars DataFrame
+#' @noRd
+kable_print_pl_df = function(x, ...) {
   .env_formatting = Sys.getenv("POLARS_FMT_TABLE_FORMATTING")
-  .env_inline_data_type = tolower(Sys.getenv("POLARS_FMT_TABLE_INLINE_COLUMN_DATA_TYPE"))
+  .env_inline_data_type = Sys.getenv("POLARS_FMT_TABLE_INLINE_COLUMN_DATA_TYPE")
+  .env_shape_below = Sys.getenv("POLARS_FMT_TABLE_DATAFRAME_SHAPE_BELOW")
 
   on.exit({
     Sys.setenv(POLARS_FMT_TABLE_FORMATTING = .env_formatting)
     Sys.setenv(POLARS_FMT_TABLE_INLINE_COLUMN_DATA_TYPE = .env_inline_data_type)
+    Sys.setenv(POLARS_FMT_TABLE_DATAFRAME_SHAPE_BELOW = .env_shape_below)
   })
 
-  if (.env_formatting %in% c("", "ASCII_MARKDOWN") && .env_inline_data_type %in% c("", "true", "1")) {
-    Sys.setenv(POLARS_FMT_TABLE_FORMATTING = "ASCII_MARKDOWN")
-    Sys.setenv(POLARS_FMT_TABLE_INLINE_COLUMN_DATA_TYPE = "1")
-  }
+  # Ensure `shape: (n, m)` above
+  Sys.unsetenv("POLARS_FMT_TABLE_DATAFRAME_SHAPE_BELOW")
+
+  Sys.setenv(POLARS_FMT_TABLE_FORMATTING = "ASCII_MARKDOWN")
+  Sys.setenv(POLARS_FMT_TABLE_INLINE_COLUMN_DATA_TYPE = "1")
 
   out = capture.output(print(x))
 
-  # Needs to insert a blank line
-  out = c(out[1], "", out[-1]) |>
-    paste(collapse = "\n")
+  if (grepl(r"(^shape:\s\(\d+,\s\d+\)$)", out[1], perl = TRUE)) {
+    # Needs to insert a blank line
+    .table_shape = c("<small>", out[1], "</small>", "")
+    .table_body = out[-1]
+  } else {
+    .table_shape = NULL
+    .table_body = out
+  }
 
-  knitr::asis_output(out)
+  c(.table_shape, "", '<div class="kable-table">', "", .table_body, "", "</div>") |>
+    paste0(collapse = "\n") |>
+    knitr::asis_output()
 }
--- tests/testthat/test-knitr.R
+++ tests/testthat/test-knitr.R
@@ -1,9 +1,13 @@
-.knit_file <- function(file_name) {
+.knit_file <- function(file_name, use = "knitr") {
   file = file.path("files", file_name)
-  output = tempfile(fileext = "md")
+  output = tempfile(fileext = ".md")
   on.exit(unlink(output))
 
-  suppressWarnings(knitr::knit(file, output, quiet = TRUE, envir = new.env()))
+  if (use == "rmarkdown") {
+    suppressWarnings(rmarkdown::render(file, output_file = output, quiet = TRUE, envir = new.env()))
+  } else {
+    suppressWarnings(knitr::knit(file, output, quiet = TRUE, envir = new.env()))
+  }
 
   readLines(output) |>
     paste0(collapse = "\n") |>
@@ -12,12 +16,13 @@
 
 test_that("Snapshot test of knitr", {
   expect_snapshot(.knit_file("dataframe.Rmd"), cran = TRUE)
-  withr::with_envvar(
-    new = c("POLARS_FMT_TABLE_INLINE_COLUMN_DATA_TYPE" = "false"),
+  withr::with_options(
+    new = list("polars.df_print" = "kable"),
     expect_snapshot(.knit_file("dataframe.Rmd"), cran = TRUE)
   )
+  expect_snapshot(.knit_file("dataframe.Rmd", use = "rmarkdown"), cran = TRUE)
   withr::with_envvar(
-    new = c("POLARS_FMT_TABLE_FORMATTING" = "DEFAULT"),
-    expect_snapshot(.knit_file("dataframe.Rmd"), cran = TRUE)
+    new = c("POLARS_FMT_TABLE_HIDE_COLUMN_DATA_TYPES" = "1"),
+    expect_snapshot(.knit_file("dataframe.Rmd", use = "rmarkdown"), cran = TRUE)
   )
 })
