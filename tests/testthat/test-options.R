c39f53c300ca8f60a6d03a39e49bcbe2377de44c
sorhawell <sorhawell@gmail.com>
2023-05-10 09:11:02 +0200

    use styler.equals

--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -1,18 +1,17 @@
 test_that("pl$options$ read-write", {
-
-  #same names and values are read
+  # same names and values are read
   expect_identical(
-    lapply(pl$options,\(f) f()),
+    lapply(pl$options, \(f) f()),
     as.list(polars_optenv)
   )
 
-  #pl$get_polars_options() get all options as in innner state
+  # pl$get_polars_options() get all options as in innner state
   expect_identical(
     pl$get_polars_options(),
     as.list(polars_optenv)
   )
 
-  #single read identical, to inner state
+  # single read identical, to inner state
   expect_identical(
     pl$options$default_maintain_order(),
     polars_optenv$default_maintain_order
@@ -27,9 +26,9 @@ test_that("pl$options$ read-write", {
       pl$options$default_maintain_order()
   )
 
-  #check if not identical, reset options, check if identical
-  expect_false(identical(old_options,as.list(polars_optenv)))
-  do.call(pl$set_polars_options,old_options)
+  # check if not identical, reset options, check if identical
+  expect_false(identical(old_options, as.list(polars_optenv)))
+  do.call(pl$set_polars_options, old_options)
   expect_identical(
     old_options,
     as.list(polars_optenv)
@@ -38,7 +37,6 @@ test_that("pl$options$ read-write", {
   # test, try write wrong value/type is rejected with error
   expect_grepl_error(
     pl$options$default_maintain_order(42),
-    c("default_maintain_order","requirement named is_bool")
+    c("default_maintain_order", "requirement named is_bool")
   )
-
 })
