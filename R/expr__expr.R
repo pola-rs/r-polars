245b20f48842dbced50bde43d0748cbba272b97b
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-06-27 18:43:08 +0200

    Accept lists in `select()`

--- R/expr__expr.R
+++ R/expr__expr.R
@@ -575,6 +575,19 @@ Expr_is_not_null = "use_extendr_wrapper"
 construct_ProtoExprArray = function(...) {
   pra = ProtoExprArray$new()
   args = list2(...)
+
+  # deal with list of expressions
+  is_list = unlist(lapply(args, \(x) {
+    class(x) == "list"
+  }))
+  if (length(is_list) > 0) is_list = which(is_list)
+  for (i in seq_along(is_list)) {
+    tmp = unlist(args[[is_list[i]]], recursive = FALSE)
+    args[[is_list[i]]] = NULL
+    args = append(tmp, args)
+  }
+  args = Filter(Negate(is.null), args)
+
   arg_names = names(args)
 
 
