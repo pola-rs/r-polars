189c96429d0652644e01040837093a13d85ddb36
eitsupi <ts1s1andn@gmail.com>
2023-06-29 08:01:18 +0000

    test: add tests for polars_info

---
+++ tests/testthat/test-info.R
@@ -0,0 +1,17 @@
+patrick::with_parameters_test_that("polars_info()features are logical",
+  {
+    expect_type(feature, "logical")
+    expect_length(feature, 1)
+  },
+  feature = polars_info()$features
+)
+
+test_that("print polars_info()", {
+  info <- polars_info()
+  # Ensure all features are FALSE for snapshot test
+  for (feature in names(info$features)) {
+    info$features[[feature]] <- FALSE
+  }
+
+  expect_snapshot(info)
+})
