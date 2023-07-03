9b1ad25cdd8a7d35aca88c22ac0f223a023d9291
Sicheng Pan <trivial@invariantspace.com>
2023-07-02 21:18:21 -0700

    Refactor code for background handler

--- src/rust/src/rthreadhandle.rs
+++ src/rust/src/rthreadhandle.rs
@@ -43,24 +43,39 @@ impl RThreadHandle<RResult<RDF>> {
     }
 }
 
+pub fn serialize_robj(robj: Robj) -> RResult<Vec<u8>> {
+    use extendr_api::*;
+    call!("serialize", &robj, NULL)
+        .map_err(RPolarsErr::from)
+        .bad_robj(&robj)
+        .when("serializing an R object")?
+        .as_raw_slice()
+        .ok_or(RPolarsErr::new())
+        .bad_robj(&robj)
+        .when("accessing raw bytes of an serialized R object")
+        .map(|bits| bits.to_vec())
+}
+
+pub fn deserialize_robj(bits: Vec<u8>) -> RResult<Robj> {
+    use extendr_api::*;
+    call!("unserialize", &bits)
+        .map_err(RPolarsErr::from)
+        .bad_val(rdbg(bits))
+        .when("deserializing an R object")
+}
+
+pub type RIPCPacket = core::option::Option<RIPCJob>;
+
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
-pub enum RIPCPacket {
+pub enum RIPCJob {
     Reval {
         raw_func: Vec<u8>,
         raw_arg: Vec<u8>,
         collector: ipc_channel::ipc::IpcSender<RResult<Vec<u8>>>,
     },
-    Done,
 }
 
-impl RIPCPacket {
-    pub fn has_job(self) -> Option<Self> {
-        match self {
-            Self::Done => None,
-            _ => Some(self),
-        }
-    }
-
+impl RIPCJob {
     pub fn handle(self) -> RResult<()> {
         use extendr_api::*;
         match self {
@@ -69,8 +84,8 @@ impl RIPCPacket {
                 raw_arg,
                 collector,
             } => {
-                let lambda = call!("unserialize", raw_func)?;
-                let arg = call!("unserialize", raw_arg)?;
+                let lambda = deserialize_robj(raw_func)?;
+                let arg = deserialize_robj(raw_arg)?;
                 let ret = lambda
                     .as_function()
                     .ok_or(RPolarsErr::new())
@@ -78,16 +93,17 @@ impl RIPCPacket {
                     .mistyped("pure R function")?
                     .call(
                         arg.as_pairlist()
+                            .or_else(|| {
+                                call!("as.pairlist", &arg)
+                                    .ok()
+                                    .and_then(|l| l.as_pairlist())
+                            })
                             .ok_or(RPolarsErr::new())
                             .bad_val(rdbg(arg))
                             .mistyped("pairlist (as R function arguments)")?,
                     )?;
-                Ok(collector.send(Ok(call!("serialize", ret, NULL)?
-                    .as_raw_slice()
-                    .unwrap()
-                    .to_vec()))?)
+                Ok(collector.send(serialize_robj(ret))?)
             }
-            Self::Done => Ok(()),
         }
     }
 }
@@ -135,7 +151,7 @@ pub fn handle_background_request(server_name: String) -> RResult<()> {
     let (tx, rx) = ipc::channel::<RIPCPacket>()?;
     rtx.send(tx)?;
 
-    while let Some(job) = rx.recv()?.has_job() {
+    while let Some(job) = rx.recv()? {
         job.handle()?;
     }
 
@@ -145,27 +161,18 @@ pub fn handle_background_request(server_name: String) -> RResult<()> {
 #[extendr]
 pub fn test_rbackgroundhandler(lambda: Robj, arg: Robj) -> RResult<Robj> {
     let handler = RBackgroundHandler::new().unwrap();
-    use extendr_api::prelude::*;
     use ipc_channel::ipc;
     let (tx, rx) = ipc::channel::<RResult<Vec<u8>>>()?;
-    let raw_func = call!("serialize", lambda, NULL)?
-        .as_raw_slice()
-        .unwrap()
-        .to_vec();
-    let raw_arg = call!("serialize", arg, NULL)?
-        .as_raw_slice()
-        .unwrap()
-        .to_vec();
 
-    handler.chan.send(RIPCPacket::Reval {
-        raw_func,
-        raw_arg,
+    handler.chan.send(Some(RIPCJob::Reval {
+        raw_func: serialize_robj(lambda)?,
+        raw_arg: serialize_robj(arg)?,
         collector: tx,
-    })?;
+    }))?;
 
     let raw_res = rx.recv()??;
-    handler.chan.send(RIPCPacket::Done)?;
-    Ok(call!("unserialize", raw_res)?)
+    handler.chan.send(None)?;
+    deserialize_robj(raw_res)
 }
 
 #[extendr]
