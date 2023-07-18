d0758f0c944752aade8ef9e5f90ae4413b870ebf
sorhawell <sorhawell@gmail.com>
2023-07-19 00:18:10 +0200

    try quote R -e arg

--- src/rust/src/rbackground.rs
+++ src/rust/src/rbackground.rs
@@ -214,7 +214,7 @@ impl RBackgroundHandler {
             .arg("-e")
             // Remove rextendr::document() if possible
             .arg(format!(
-                "polars:::handle_background_request(\"{server_name}\") |> invisible()"
+                "'polars:::handle_background_request(\"{server_name}\") |> invisible()'"
             ))
             .stdin(Stdio::null())
             .stdout(Stdio::null())
