3c4b5efaae0e5e721170f047f53491d0d9ef310e
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-08-04 18:28:03 +0200

    Update tests/testthat/test-extendr-meta.R

--- tests/testthat/test-extendr-meta.R
+++ tests/testthat/test-extendr-meta.R
@@ -1,15 +1,15 @@
 test_that("clone_robj + mem_adress", {
 
-  #clone mutatble
+  # clone mutable
   env  = new.env(parent = emptyenv())
-  env2 = polars:::clone_robj(env)
+  env2 = clone_robj(env)
   env$foo = 42
   expect_identical(env,env2)
   expect_identical(pl$mem_address(env), pl$mem_address(env2))
 
-  #clone immutatble, not the same
+  # clone immuatble, not the same
   l = list()
-  l2 = polars:::clone_robj(l)
+  l2 = clone_robj(l)
   l$foo = 42
   expect_identical(l,list(foo = 42))
   expect_identical(l2,list())
