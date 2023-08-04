c66702c49f038e5d7b1e8bb348e3d92d63c63211
Søren Havelund Welling <sorhawell@gmail.com>
2023-08-05 01:20:06 +0200

    Update tests/testthat/test-lazy_profile.R
    Co-authored-by: Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>

--- tests/testthat/test-lazy_profile.R
+++ tests/testthat/test-lazy_profile.R
@@ -1,5 +1,5 @@
 test_that("can modify lazy profile settings", {
-  #some way to check if .pr$LazyFrame$optimization_toggle works
+  # TODO: some way to check if .pr$LazyFrame$optimization_toggle works
 
   #toggle settings
   #read back settings
