598fc18ab083482b75a3ec18d559971dd69748e0
sorhawell <sorhawell@gmail.com>
2023-08-11 11:50:36 +0200

    rust-polars version to pl$polars_info()

--- R/info.R
+++ R/info.R
@@ -1,3 +1,17 @@
+# get version of rust-polars version from Cargo.lock at package build time
+RUST_POLARS_VERSION = (\() {
+  Cargo.lock = readLines("./src/rust/Cargo.lock")
+  polars.idx = which(Cargo.lock == r"{name = "polars"}")[1]
+  if(is.finite(polars.idx)) for(i in polars.idx + 1:10) {
+    this_line = Cargo.lock[i]
+    if(i > length(Cargo.lock) || this_line == "[[package]]") break()
+    if(isTRUE(substr(this_line,1,7) == "version")) return(substr(this_line,12,nchar(this_line)-1))
+  }
+  warning("failed to find RUST_POLARS_VERSION version")
+  "unknown"
+})()
+
+
 #' Report information of the package
 #'
 #' @return A list with information of the package
@@ -8,6 +22,7 @@ pl$polars_info = function() {
   # Similar to arrow::arrow_info()
   out = list(
     version = utils::packageVersion("polars"),
+    rust_polars = RUST_POLARS_VERSION,
     features = FeatureInfo$new()$to_r()
   )
 
@@ -27,6 +42,8 @@ print.polars_info = function(x, ...) {
     cat("\n")
   }
 
-  cat("R Polars package version: ", format(x$version), "\n\n", sep = "")
+  cat("r-polars package version : ", format(x$version), "\n", sep = "")
+  cat("rust-polars crate version: ", format(x$rust_polars), "\n", sep = "")
+  cat("\n")
   print_key_values("Features", unlist(x$features))
 }
