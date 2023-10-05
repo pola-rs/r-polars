ae93182cd10d6d98ec59ccb1f2cea6f0872c7efe
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-10-05 19:25:53 +0200

    Implement `$cat$get_categories()`

--- tests/testthat/test-expr_categorical.R
+++ tests/testthat/test-expr_categorical.R
@@ -10,3 +10,13 @@ test_that("set_ordering", {
   # https://github.com/pola-rs/polars/issues/6513
   e$cat$set_ordering("lexical")$sort()$lit_to_s()
 })
+
+test_that("get_categories", {
+  df = pl$DataFrame(
+    cats = factor(c("z", "z", "k", "a", "b"))
+  )
+  expect_identical(
+    df$select(pl$col("cats")$cat$get_categories())$to_data_frame(),
+    data.frame(cats = c("z", "k", "a", "b"))
+  )
+})
