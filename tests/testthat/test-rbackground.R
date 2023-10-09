a66b6a92fa49d6c2677f2736ee1883300cdfe047
eitsupi <ts1s1andn@gmail.com>
2023-10-09 08:18:30 +0000

    auto formatting

--- tests/testthat/test-rbackground.R
+++ tests/testthat/test-rbackground.R
@@ -1,7 +1,7 @@
 lf = pl$LazyFrame(data.frame(x = 1:10, y = 11:20))
 
 test_that("Test collecting LazyFrame in background", {
-  skip_if_not(Sys.getenv("CI") == 'true')
+  skip_if_not(Sys.getenv("CI") == "true")
   compute = lf$select(pl$col("x") * pl$col("y"))
   res_bg = compute$collect_in_background()$join()
   expect_equal(res_bg$to_data_frame(), compute$collect()$to_data_frame())
@@ -12,7 +12,7 @@ test_that("Test collecting LazyFrame in background", {
 })
 
 test_that("Test using $map() in background", {
-  skip_if_not(Sys.getenv("CI") == 'true')
+  skip_if_not(Sys.getenv("CI") == "true")
   # change capacity
   pl$set_options(rpool_cap = 0)
   expect_equal(pl$options$rpool_cap, 0)
@@ -65,7 +65,7 @@ test_that("Test using $map() in background", {
 
 
 test_that("reset rpool_cap", {
-  skip_if_not(Sys.getenv("CI") == 'true')
+  skip_if_not(Sys.getenv("CI") == "true")
   pl$reset_options()
   orig = pl$options$rpool_cap
   pl$set_options(rpool_cap = orig + 1)
@@ -76,25 +76,30 @@ test_that("reset rpool_cap", {
 
 
 test_that("rpool errors", {
-  skip_if_not(Sys.getenv("CI") == 'true')
-  ctx =  pl$set_options(rpool_cap = c(1, 2)) |> get_err_ctx()
+  skip_if_not(Sys.getenv("CI") == "true")
+  ctx = pl$set_options(rpool_cap = c(1, 2)) |> get_err_ctx()
   expect_identical(ctx$BadArgument, "rpool_cap")
-  expect_true(startsWith(ctx$TypeMismatch,"i64"))
+  expect_true(startsWith(ctx$TypeMismatch, "i64"))
 
   ctx = pl$set_options(rpool_cap = -1) |> get_err_ctx()
   expect_identical(ctx$ValueOutOfScope, "cannot be less than zero")
 
-  ctx = {polars_optenv$rpool_active <- 0} |> get_err_ctx()
-  expect_true(endsWith(ctx$PlainErrorMessage,"rpool_active cannot be set directly"))
-
+  ctx =
+    {
+      polars_optenv$rpool_active = 0
+    } |> get_err_ctx()
+  expect_true(endsWith(ctx$PlainErrorMessage, "rpool_active cannot be set directly"))
 })
 
-test_that("reduce cap and active while jobs in queue",{
-  skip_if_not(Sys.getenv("CI") == 'true')
+test_that("reduce cap and active while jobs in queue", {
+  skip_if_not(Sys.getenv("CI") == "true")
   pl$set_options(rpool_cap = 0)
   pl$set_options(rpool_cap = 3)
-  l_expr = lapply(1:5,\(i) {
-    pl$lit(i)$map(\(x) {Sys.sleep(.4); -i}, in_background = TRUE)$alias(paste0("lit_",i))
+  l_expr = lapply(1:5, \(i) {
+    pl$lit(i)$map(\(x) {
+      Sys.sleep(.4)
+      -i
+    }, in_background = TRUE)$alias(paste0("lit_", i))
   })
   lf = pl$LazyFrame()$select(l_expr)
   handle = lf$collect(collect_in_background = TRUE)
@@ -110,4 +115,4 @@ test_that("reduce cap and active while jobs in queue",{
   )
 
   pl$reset_options()
-} )
+})
