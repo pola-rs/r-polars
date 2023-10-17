0bb4624880c8e0df685a894f3aeebb65e0d52cb2
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-10-17 14:51:32 +0200

    typo

--- R/extendr-wrappers.R
+++ R/extendr-wrappers.R
@@ -649,7 +649,7 @@ Expr$dt_truncate <- function(every, offset, ambiguous) .Call(wrap__Expr__dt_trun
 
 Expr$dt_round <- function(every, offset) .Call(wrap__Expr__dt_round, self, every, offset)
 
-Expr$dt_time <- function(every, offset) .Call(wrap__Expr__dt_time, self)
+Expr$dt_time <- function() .Call(wrap__Expr__dt_time, self)
 
 Expr$dt_combine <- function(time, tu) .Call(wrap__Expr__dt_combine, self, time, tu)
 
