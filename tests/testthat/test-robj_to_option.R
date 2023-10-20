87c8f1d4be1da4446544927910da7275a677906a
sorhawell <sorhawell@gmail.com>
2023-10-20 22:01:31 +0200

    update utest

--- tests/testthat/test-robj_to_option.R
+++ tests/testthat/test-robj_to_option.R
@@ -1,12 +1,23 @@
-test_that("robj_to_roption",{
+test_that("robj_to_roption", {
 
     # gets first value in char vec
-    expect_identical(test_robj_to_roption(c("a","b"))$ok, "a")
+    expect_identical(test_robj_to_roption(c("a","b", NA))$ok, "a")
 
-    # gets only string value
+    # ... or string
     expect_identical(test_robj_to_roption(c("a"))$ok, "a")
 
-    # gets
-    expect_identical(test_robj_to_roption(NA_character_) )
+    # NA chr not allowed as first element
+    ctx = test_robj_to_roption(NA_character_)$err$contexts()
+    expect_identical(ctx$PlainErrorMessage,  "an R option/choice should not be a NA_character")
 
+    # empty chr vec not allowed as first element
+    ctx = test_robj_to_roption(character())$err$contexts()
+    expect_identical(
+      ctx$PlainErrorMessage,
+      "an R option/choice character vector cannot have zero length"
+    )
+
+    # non char vec / string not allowed
+    ctx = test_robj_to_roption(42)$err$contexts()
+    expect_identical(ctx$PlainErrorMessage,  "an R option/choice should be a character vector")
 })
