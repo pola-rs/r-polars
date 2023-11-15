1556e6faa3ebde7051a335247266b4a09cedd946
sorhawell <sorhawell@gmail.com>
2023-11-15 01:30:39 +0100

    split up utest

--- tests/testthat/test-parquet.R
+++ tests/testthat/test-parquet.R
@@ -1,7 +1,4 @@
-
-
-test_that("scan read parquet", {
-
+test_that("plain scan read parquet", {
   tmpf = tempfile()
   on.exit(unlink(tmpf))
   lf_exp = pl$LazyFrame(mtcars)
@@ -19,14 +16,36 @@ test_that("scan read parquet", {
     pl$read_parquet(tmpf)$to_data_frame(),
     df_exp
   )
+})
 
-  # with row count
+
+test_that("scan read parquet - test arg rowcount", {
+  tmpf = tempfile()
+  on.exit(unlink(tmpf))
+  lf_exp = pl$LazyFrame(mtcars)
+  lf_exp$sink_parquet(tmpf, compression = "snappy")
+  df_exp = lf_exp$collect()$to_data_frame()
+
+  expect_identical(
+    pl$scan_parquet(tmpf, row_count_name = "rc", row_count_offset = 5)$collect()$to_data_frame(),
+    data.frame(rc = as.numeric(5:36), df_exp)
+  )
+
   expect_identical(
     pl$read_parquet(tmpf, row_count_name = "rc", row_count_offset = 5)$to_data_frame(),
     data.frame(rc = as.numeric(5:36), df_exp)
   )
+})
 
-  # check all parallel strategies work
+
+test_that("scan read parquet - parallel strategies", {
+  tmpf = tempfile()
+  on.exit(unlink(tmpf))
+  lf_exp = pl$LazyFrame(mtcars)
+  lf_exp$sink_parquet(tmpf, compression = "snappy")
+  df_exp = lf_exp$collect()$to_data_frame()
+
+  # check all parallel strategies produce same result
   for (choice in c("auto", "COLUMNS", "None", "rowGroups")) {
     expect_identical(
       pl$read_parquet(tmpf, parallel = choice)$to_data_frame(),
@@ -40,4 +59,5 @@ test_that("scan read parquet", {
   expect_identical(ctx$BadArgument, "parallel")
   ctx = pl$read_parquet(tmpf, parallel = 42) |> get_err_ctx()
   expect_identical(ctx$NotAChoice, "input is not a character vector")
+
 })
