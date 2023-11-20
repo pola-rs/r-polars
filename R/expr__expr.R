0e5534124c018d7e427a4f6d8750d888dae2e433
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-11-20 08:19:21 +0100

    more docs

--- R/expr__expr.R
+++ R/expr__expr.R
@@ -3361,6 +3361,20 @@ Expr_peak_max = function() {
 
 #' Create rolling groups based on a time or numeric column
 #'
+#' @description
+#' If you have a time series `<t_0, t_1, ..., t_n>`, then by default the windows
+#' created will be:
+#' * (t_0 - period, t_0]
+#' * (t_1 - period, t_1]
+#' * …
+#' * (t_n - period, t_n]
+#'
+#' whereas if you pass a non-default offset, then the windows will be:
+#' * (t_0 + offset, t_0 + offset + period]
+#' * (t_1 + offset, t_1 + offset + period]
+#' * …
+#' * (t_n + offset, t_n + offset + period]
+#'
 #' @param index_column Column used to group based on the time window. Often of
 #' type Date/Datetime. This column must be sorted in ascending order. If this
 #' column represents an index, it has to be either Int32 or Int64. Note that
@@ -3374,6 +3388,33 @@ Expr_peak_max = function() {
 #' expensive so if you are sure the data within the `index_column` is sorted, you
 #' can set this to `FALSE` but note that if the data actually is unsorted, it
 #' will lead to incorrect output.
+#'
+#' @details
+#' The period and offset arguments are created either from a timedelta, or by
+#' using the following string language:
+#' * 1ns (1 nanosecond)
+#' * 1us (1 microsecond)
+#' * 1ms (1 millisecond)
+#' * 1s (1 second)
+#' * 1m (1 minute)
+#' * 1h (1 hour)
+#' * 1d (1 calendar day)
+#' * 1w (1 calendar week)
+#' * 1mo (1 calendar month)
+#' * 1q (1 calendar quarter)
+#' * 1y (1 calendar year)
+#' * 1i (1 index count)
+#'
+#' Or combine them: "3d12h4m25s" # 3 days, 12 hours, 4 minutes, and 25 seconds
+#'
+#' By "calendar day", we mean the corresponding time on the next day (which may
+#' not be 24 hours, due to daylight savings). Similarly for "calendar week",
+#' "calendar month", "calendar quarter", and "calendar year".
+#'
+#' In case of a rolling operation on an integer column, the windows are defined
+#' by:
+#' * "1i" # length 1
+#' * "10i" # length 10
 #'
 #' @return Expr
 #'
@@ -3397,8 +3438,7 @@ Expr_peak_max = function() {
 #' # offset = "1d", we start the window one day after the value in "dt", and
 #' # then we add a 2-day window relative to the window start.
 #' df$with_columns(
-#'   sum_a_offset1 = pl$sum("a")$rolling(index_column = "dt", period = "2d", offset = "1d"),
-#'   sum_a_offset2 = pl$sum("a")$rolling(index_column = "dt", period = "2d", offset = "2d")
+#'   sum_a_offset1 = pl$sum("a")$rolling(index_column = "dt", period = "2d", offset = "1d")
 #' )
 Expr_rolling = function(index_column, period, offset = NULL,
                         closed = "right", check_sorted = TRUE) {
