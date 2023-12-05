71e988069ffaf2cc21c7d63a3174be0180e1bfff
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-12-05 13:46:03 +0100

    Implement `$str$to_date()`, `$str$to_time()`, `$str$to_datetime()` (#558)

--- tests/testthat/test-expr_string.R
+++ tests/testthat/test-expr_string.R
@@ -81,6 +81,55 @@ test_that("str$strptime time", {
   )
 })
 
+test_that("$str$to_date", {
+  out = pl$lit(c("2009-01-02", "2009-01-03", "2009-1-4"))$
+    str$to_date()$to_r()
+  expect_equal(
+    out,
+    as.Date(c("2009-01-02", "2009-01-03", "2009-01-04"))
+  )
+  expect_error(
+    pl$lit(c("2009-01-02", "2009-01-03", "2009-1-4"))$
+      str$to_date(format = "%Y / %m / %d")$to_r()
+  )
+  expect_equal(
+    pl$lit(c("2009-01-02", "2009-01-03", "2009-1-4"))$
+      str$to_date(format = "%Y / %m / %d", strict = FALSE)$to_r(),
+    as.Date(rep(NA_character_, 3))
+  )
+})
+
+test_that("$str$to_time", {
+  out = pl$lit(c("01:20:01", "28:00:02", "03:00:02"))$
+    str$to_time(strict = FALSE)$to_r()
+  expect_equal(
+    out,
+    pl$PTime(c("01:20:01", "28:00:02", "03:00:02"), tu = "ns")
+  )
+  expect_error(
+    ppl$lit(c("01:20:01", "28:00:02", "03:00:02"))$
+      str$to_time()
+  )
+})
+
+test_that("$str$to_datetime", {
+  out = pl$lit(c("2009-01-02 01:00", "2009-01-03 02:00", "2009-1-4 03:00"))$
+    str$to_datetime(time_zone = "UTC")$to_r()
+  expect_equal(
+    out,
+    as.POSIXct(c("2009-01-02 01:00:00", "2009-01-03 02:00:00", "2009-01-04 03:00:00"), tz = "UTC")
+  )
+  expect_error(
+    pl$lit(c("2009-01-02 01:00", "2009-01-03 02:00", "2009-1-4"))$
+      str$to_date(format = "%Y / %m / %d")$to_r()
+  )
+  expect_equal(
+    pl$lit(c("2009-01-02 01:00", "2009-01-03 02:00", "2009-1-4"))$
+      str$to_date(format = "%Y / %m / %d", strict = FALSE)$to_r(),
+    as.Date(rep(NA_character_, 3))
+  )
+})
+
 test_that("str$len_bytes str$len_chars", {
   test_str = c("Café", NA, "345", "東京") |> enc2utf8()
   Encoding(test_str)
@@ -102,8 +151,6 @@ test_that("str$len_bytes str$len_chars", {
     )
   )
 })
-
-
 
 test_that("str$concat", {
   # concatenate a Series of strings to a single string
