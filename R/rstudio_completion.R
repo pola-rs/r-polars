4afa146865743105392d5c076afa5c1113c3486f
sorhawell <sorhawell@gmail.com>
2023-12-17 23:54:10 +0100

    work on function arg completion

--- R/rstudio_completion.R
+++ R/rstudio_completion.R
@@ -1,86 +1,114 @@
 
+.dev = new.env(parent = emptyenv())
 
-activate_pl_autocomplete <- function(f) {
+if(interactive()) .dev = polars:::.dev
+
+.dev$eval_lhs_string = function(string, envir) {
+  lhs = NULL
+  tryCatch(
+    {
+      base::setTimeLimit(elapsed = 3)
+      lhs = eval(parse(text = string),envir = envir)
+      TRUE
+    },
+    error = function(e) {
+      message("could no auto complete syntax because:")
+      if(inherits(e, "RPolarsError")) {
+        message(e$rcall(string))
+      } else if (
+        inherits(e, "simpleError") &&
+        identical(e$message, "reached elapsed time limit")
+      ) {
+        message(paste0("completion timelimit (3s) exceeded. Use only for small eager or lazy."))
+      } else {
+        message(e)
+      }
+    }
+  )
+  base::setTimeLimit(elapsed = Inf)
+  lhs
+}
+
+.dev$is_polars_related_type = function(x) {
+  inherits(
+    x,
+    c(
+      "RPolarsLazyFrame", "RPolarsSeries", "RPolarsLazyGroupBy", "RPolarsDataType",
+      "RPolarsExpr", "RPolarsDataFrame", "RPolarsWhen", "RPolarsThen",
+      "RPolarsChainedWhen", "RPolarsChainedThen", "RPolarsSQLContext",
+      "method_environment"
+    )
+  )
+}
+
+.dev$helpHandler = function (type = "completion", topic, source, ...) {
+  #browser()
+  print("hello helper")
+  print(type)
+  print(topic)
+  if (type == "completion") {
+      list(
+        title = topic, signature = NULL, returns = "lots of stuff",
+        description = "description", details = "details", sections = "sections"
+      )
+  }
+  else if (type == "parameter") {
+       "here to help <3 parameter"
+  }
+  else if (type == "url") {
+      "here to help <3 url"
+  }
+}
+
+activate_pl_autocomplete <- function() {
   rs = as.environment("tools:rstudio")
   local({
+
+    .rs.getCompletionsFunction_orig <- .rs.getCompletionsFunction
+    .rs.getCompletionsFunction <- function(
+      token, string, functionCall, numCommas, envir = parent.frame(),
+      object = .rs.resolveObjectFromFunctionCall(functionCall, envir)) {
+      if(isFALSE(object)) {
+        lhs = polars:::.dev$eval_lhs_string(string, envir)
+        object = lhs
+        string = ""
+      }
+
+      .rs.getCompletionsFunction_orig(
+        token, string, functionCall, numCommas, envir = envir, object = object
+      )
+    }
+
     if(exists(".rs.getCompletionsDollar_orig",))
       stop("cannot stack multiple rules with this impl")
     .rs.getCompletionsDollar_orig <- .rs.getCompletionsDollar
     .rs.getCompletionsDollar <- function(token, string, functionCall, envir, isAt) {
-      #browser()
-      lhs = NULL
-      tryCatch(
-        {
-          lhs = eval(parse(text = string),envir = envir)
-          TRUE
-        },
-        error = function(e) {
-          message("could no auto complete polars syntax because following previous error")
-          if(inherits(e, "RPolarsError")) {
-            print(e$rcall(string))
-          } else {
-            print(e)
-          }
-        }
-      )
-
-      if(
-        !is.null(lhs) &&
-        inherits(
-          lhs,
-          c(
-            "RPolarsLazyFrame", "RPolarsSeries", "RPolarsLazyGroupBy", "RPolarsDataType",
-            "RPolarsExpr", "RPolarsDataFrame", "RPolarsWhen", "RPolarsThen",
-            "RPolarsChainedWhen", "RPolarsChainedThen", "RPolarsSQLContext"
-          )
-        )
-      ) {
-        #browser()
-        print(str(lhs))
-
-
-        #modified from reticulate:::help_handler
-        helpHandler = function (type = "completion", topic, source, ...) {
-          #browser()
-          print("hello helper")
-          print(type)
-          print(topic)
-          if (type == "completion") {
-              list(
-                title = topic, signature = NULL, returns = "lots of stuff",
-                description = "description", details = "details", sections = "sections"
-              )
-          }
-          else if (type == "parameter") {
-               "here to help <3 parameter"
-          }
-          else if (type == "url") {
-              "here to help <3 url"
-          }
-        }
-
-
-        #browser()
-        completions = .rs.makeCompletions(
-          token = token, results = gsub("\\(|\\)","",.DollarNames(lhs,token)), excludeOtherCompletions = TRUE,
-          quote = FALSE, #helpHandler = l,
-          context = .rs.acContextTypes$DOLLAR,
-          type = .rs.acCompletionTypes$FUNCTION, meta = "more info",
-        )
-        return(completions)
-      } else {
+      lhs = polars:::.dev$eval_lhs_string(string, envir)
+      if(!  polars:::.dev$is_polars_related_type(lhs)) {
         .rs.getCompletionsDollar_orig(token, string, functionCall, envir, isAt)
       }
-      #browser()
-
+      results = gsub("\\(|\\)","",.DollarNames(lhs,token))
+      types = sapply(
+        results,
+        function(x) .rs.getCompletionType(eval(substitute(`$`(lhs,x), list(x=x))))
+      )
+      .rs.makeCompletions(
+        token = token, results = results, excludeOtherCompletions = TRUE, packages = "polars",
+        quote = FALSE, helpHandler = FALSE,
+        context = .rs.acContextTypes$DOLLAR, suggestOnAccept ="foobar",
+        type = types, meta = "more info",
+      )
     } # end new dollar f
   }, envir = rs) #end local
 }
 
+# # # #
+# polars:::activate_pl_autocomplete()
+# # # #
+# # # #
+# library(polars)
+# # # #
+# # # #
+# pl$col("a")$alias("blop")$sum()$alias(
 
-activate_pl_autocomplete()
-
-
-library(polars)
-pl$col("a")$alias("blop")$sum()$abs()$all()$abs(
 
