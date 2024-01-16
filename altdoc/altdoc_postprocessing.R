47cd85135512a7fd18f76a7e2ea8e81cb55cd322
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-01-16 19:06:42 +0100

    revert change in altdoc script

--- altdoc/altdoc_postprocessing.R
+++ altdoc/altdoc_postprocessing.R
@@ -1,91 +1,83 @@
 ### To be run after altdoc::render_docs()
-###
+
+list_man_html = list.files("docs/man",
+                           pattern = "\\.html$", full.names = TRUE,
+                           recursive = TRUE
+)
+
 ### Make the "Usage" section prettier (if there is one):
 ### DataFrame_describe(...)   ->   <DataFrame>$describe()
 
-list_man_html = list.files("docs/man",
-  pattern = "\\.html$", full.names = TRUE,
-  recursive = TRUE
+classes = c(
+  "Series", "DataFrame", "LazyFrame", "GroupBy",
+  "LazyGroupBy", "IO", "RField", "RThreadHandle", "SQLContext", "S3",
+  "Expr", "pl"
 )
 
-patterns_replacements = rbind(
-  c("DataFrame_", "<DataFrame>$"),
-  c("DynamicGroupBy_", "<DynamicGroupBy>$"),
-  c("Expr_", "<Expr>$"),
-  c("ExprBin_", "<Expr>$bin$"),
-  c("ExprCat_", "<Expr>$cat$"),
-  c("ExprDT_", "<Expr>$dt$"),
-  c("ExprList_", "<Expr>$list$"),
-  c("ExprMeta_", "<Expr>$meta$"),
-  c("ExprName_", "<Expr>$name$"),
-  c("ExprStr_", "<Expr>$str$"),
-  c("ExprStruct_", "<Expr>$struct$"),
-  c("GroupBy_", "<GroupBy>$"),
-  # file names are "IO_read_", function names are "pl_read_"
-  c("(IO|pl)_read_", "pl$read_"),
-  # file names are "IO_scan_", function names are "pl_scan_"
-  c("(IO|pl)_scan_", "pl$scan_"),
-  # file names are "IO_sink_", function names are "LazyFrame_sink_"
-  c("(IO|LazyFrame)_sink_", "<LazyFrame>$sink_"),
-  # file names are "IO_write_", function names are "DataFrame_write_"
-  c("(IO|DataFrame)_write_", "<DataFrame>$write_"),
-  c("LazyFrame_", "<LazyFrame>$"),
-  c("LazyGroupBy_", "<LazyGroupBy>$"),
-  c("pl_", "pl$"),
-  # Category "DataType" in the sidebar, but called with "pl$"
-  c("DataType_", "pl$"),
-  c("RField_", "<RField>$"),
-  c("RThreadHandle_", "<RThreadHandle>$"),
-  c("Series_", "<Series>$"),
-  c("SQLContext_", "<SQLContext>$")
-) |> as.data.frame()
+to_modify = grep(
+  paste0("/", paste(classes, collapse = "|")),
+  list_man_html,
+  value = TRUE
+)
 
-colnames(patterns_replacements) = c("pattern", "replacement")
-
-
-
-replace_in_usage = function(txt, which_class, replacement) {
-  usage_section = grep("<h2 id=\"usage\">Usage</h2>", txt)
-  if (length(usage_section) == 1) {
-      before_usage_idx = 1:usage_section
-      usage = txt[-before_usage_idx]
-      after_usage_idx = grep("</h2>", usage)[1]
-      usage = usage[1:(after_usage_idx - 1)]
-      new_usage = gsub(which_class, replacement, usage)
-      c(
-        txt[before_usage_idx],
-        new_usage,
-        txt[(length(before_usage_idx) + length(usage) + after_usage_idx):length(txt)]
-      )
-  } else {
-    txt
-  }
-}
-
-
-
-for (i in list_man_html) {
-  which_class = paste0(gsub("docs/man/([^_]+).*$", "\\1", i, perl = TRUE), "_")
+for (i in to_modify) {
+  which_class = gsub("docs/man/([^_]+).*$", "\\1", i, perl = TRUE)
   orig = readLines(i, warn = FALSE)
 
   if (!any(grepl("<h2 id=\"usage\">Usage</h2>", orig))) {
     next
   }
 
-  replacement = patterns_replacements[
-    patterns_replacements$pattern == which_class,
-    "replacement"
-  ] |>
-    gsub("<", "&lt;", x = _) |>
-    gsub(">", "&gt;", x = _)
-
-  if (length(replacement) == 1) {
-    new = replace_in_usage(orig, which_class, replacement)
+  # IO functions are DataFrame or LazyFrame methods
+  if (which_class == "IO") {
+    if (any(grepl("<code class='language-R'>LazyFrame_sink", orig))) {
+      which_class <<- "LazyFrame"
+    } else if (any(grepl("<code class='language-R'>DataFrame_write", orig))) {
+      which_class <<- "DataFrame"
+    }
   }
 
-  # fix escaping of angle brackets
+  # prefix with pl$ for read/scan
+  if (which_class == "IO") {
+    which_input = if (any(grepl("<code class='language-R'>read_", orig))) {
+      "read"
+    } else if (any(grepl("<code class='language-R'>scan_", orig))) {
+      "scan"
+    } else {
+      ""
+    }
+    new = gsub(
+      paste0("<code class='language-R'>", which_input, "_"),
+      paste0("<code class='language-R'>pl$", which_input, "_"),
+      orig
+    )
+  } else if (which_class == "pl") {
+    new = gsub(
+      "<code class='language-R'>pl_",
+      "<code class='language-R'>pl$",
+      orig
+    )
+  } else if (which_class %in% c(
+    "ExprBin", "ExprCat", "ExprDT", "ExprList",
+    "ExprMeta", "ExprName", "ExprStr", "ExprStruct"
+  )) {
+    subns = tolower(gsub("Expr", "", which_class))
+
+    new = gsub(
+      paste0("<code class='language-R'>", which_class, "_"),
+      paste0("<code class='language-R'>&lt;Expr&gt;$", subns, "$"),
+      orig
+    )
+  } else {
+    new = gsub(
+      paste0("<code class='language-R'>", which_class, "_"),
+      paste0("<code class='language-R'>&lt;", which_class, "&gt;$"),
+      orig
+    )
+  }
+
+  # fix escaping of left-angle brackets (not needed for right-angle brackets)
   new = gsub("\\\\&lt;", "&lt;", new)
-  new = gsub("\\\\&gt;", "&gt;", new)
 
   writeLines(new, i)
 }
