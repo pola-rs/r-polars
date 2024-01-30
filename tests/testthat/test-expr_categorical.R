dcc5a426859e03ca16a0a7bffb91f5741f92891d
etiennebacher <etienne.bacher@protonmail.com>
2024-01-30 22:46:20 +0100

    uncomment test for $set_ordering()

--- tests/testthat/test-expr_categorical.R
+++ tests/testthat/test-expr_categorical.R
@@ -6,9 +6,10 @@ test_that("set_ordering", {
     factor(c("z", "z", "k", "a", "b"))
   )
 
-  # TODO lexical ordering is currently broken, follow issue
-  # https://github.com/pola-rs/polars/issues/6513
-  e$cat$set_ordering("lexical")$sort()$to_series()
+  expect_identical(
+    e$cat$set_ordering("lexical")$sort()$to_series()$to_r(),
+    factor(c("a", "b", "k", "z", "z"))
+  )
 })
 
 test_that("get_categories", {
