1fb0923b4391ebd39ff39c2be44f1b319cbf60b3
eitsupi <ts1s1andn@gmail.com>
2024-02-08 13:25:15 +0000

    refactor!: change the `polars_info()` output structure

--- tests/testthat/test-polars_info.R
+++ tests/testthat/test-polars_info.R
@@ -10,8 +10,8 @@ test_that("print polars_info()", {
   info = polars_info()
 
   # Ensure static version for snapshot test
-  info$version = package_version("999.999.999")
-  info$rust_polars = package_version("999.999.999")
+  info$versions$r_package = package_version("999.999.999")
+  info$versions$rust_crate = package_version("999.999.999")
 
   # Ensure the thread_pool_size is 1 for snapshot test
   info$thread_pool_size = 1
