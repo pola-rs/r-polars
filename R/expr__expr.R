3b140167da8838fde000c6be385b94a74032618c
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-03-19 08:56:49 +0100

    init

--- R/expr__expr.R
+++ R/expr__expr.R
@@ -3074,23 +3074,6 @@ Expr_to_r = function(df = NULL, i = 0, ..., int64_conversion = polars_options()$
   }
 }
 
-#' Convert an Expr to R output
-#'
-#' This is mostly useful to debug an expression. It evaluates the Expr in an
-#' empty DataFrame and return the first Series to R. This is an alias for
-#' `$to_r()`.
-#' @param expr An Expr to evaluate.
-#' @param df If `NULL` (default), it evaluates the Expr in an empty DataFrame.
-#' Otherwise, provide a DataFrame that the Expr should be evaluated in.
-#' @param i Numeric column to extract. Default is zero (which gives the first
-#' column).
-#' @return R object
-#' @examples
-#' pl$expr_to_r(pl$lit(1:3))
-pl_expr_to_r = function(expr, df = NULL, i = 0) {
-  wrap_e(expr)$to_r(df, i)
-}
-
 #' Value counts
 #' @description
 #' Count all unique values and create a struct mapping value to count.
