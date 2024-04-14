5f9fb8155729237031b128d53ca6306444bb4f42
etiennebacher <etienne.bacher@protonmail.com>
2024-04-14 16:02:53 -0400

    duplicated test

--- tests/testthat/test-expr_expr.R
+++ tests/testthat/test-expr_expr.R
@@ -999,8 +999,6 @@ test_that("sort_by", {
 
   expect_grepl_error(pl$lit(1:4)$sort_by(1)$to_r(), "different length")
   expect_grepl_error(pl$lit(1:4)$sort_by("blop")$to_r(), "field not found")
-  expect_grepl_error(pl$lit(1:4)$sort_by("blop")$to_r(), "field not found")
-  expect_grepl_error(pl$lit(1:4)$sort_by(df)$to_r(), "not convertible into.* Expr")
   expect_grepl_error(pl$lit(1:4)$sort_by(df)$to_r(), "not convertible into.* Expr")
 
   # this test is minimal, if polars give better documentation on behavior, expand the test.
