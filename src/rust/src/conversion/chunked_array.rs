232958a56077d1e5f2e6dee368ebd4a4445a30c2
eitsupi <50911393+eitsupi@users.noreply.github.com>
2024-06-29 14:34:15 +0000

    refactor: Date type export to R refactoring

--- src/rust/src/conversion/chunked_array.rs
+++ src/rust/src/conversion/chunked_array.rs
@@ -62,3 +62,19 @@ impl From<Wrap<&StringChunked>> for Sexp {
         sexp.into()
     }
 }
+
+impl From<Wrap<&DateChunked>> for Sexp {
+    fn from(ca: Wrap<&DateChunked>) -> Self {
+        let ca = ca.0;
+        let mut sexp = OwnedRealSexp::new(ca.len()).unwrap();
+        let _ = sexp.set_class(&["Date"]);
+        for (i, v) in ca.into_iter().enumerate() {
+            if let Some(v) = v {
+                let _ = sexp.set_elt(i, v as f64);
+            } else {
+                let _ = sexp.set_na(i);
+            }
+        }
+        sexp.into()
+    }
+}
