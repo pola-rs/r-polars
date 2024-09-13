d5d28c30bacb3ace1dd8d30dac2da9e7ed2036f0
eitsupi <ts1s1andn@gmail.com>
2024-09-13 15:40:52 +0000

    docs: more document about literal values and `pl$lit()`

--- R/as_polars_expr.R
+++ R/as_polars_expr.R
@@ -1,13 +1,16 @@
 # Same as Python Polars' `parse_into_expression`
 # TODO: link to Expr class page
 # TODO: link to data type page
-# TODO: link to `pl__lit`
 # TODO: link to `expr__first`
 # TODO: link to `pl__col`
 #' Create a Polars Expression from an R object
 #'
 #' The [as_polars_expr()] function creates a polars Expression from various R objects.
 #' This function is used internally by various polars functions that accept Expressions.
+#' In most cases, users should use [`pl$lit()`][pl__lit] instead of this function, which is
+#' a shorthand for `as_polars_expr(x, as_lit = TRUE)`.
+#' (In other words, this function can be considered as an internal implementation to realize
+#' the `lit` function of the Polars API in other languages.)
 #'
 #' Because R objects are typically mapped to [Series], this function often calls [as_polars_series()] internally.
 #' However, unlike R, Polars has scalars of length 1, so if an R object is converted to a [Series] of length 1,
@@ -27,17 +30,39 @@
 #' If the `as_lit` argument is `FALSE` (default), this function will call `pl$col()` and
 #' the character vector is treated as column names.
 #'
+#' # Literal scalar mapping
+#'
+#' Since R has no scalar class, each of the following types of length 1 cases is specially
+#' converted to a scalar literal.
+#'
+#' - character: String
+#' - logical: Boolean
+#' - integer: Int32
+#' - double: Float64
+#'
+#' and, the [raw] type vector is converted to a Binary scalar.
+#'
+#' - raw: Binary
+#'
+#' `NULL` is converted to a `null` literal.
+#'
+#' - `NULL`: Null
+#'
+#' Other scalar values cannot be created directly, so we need to `$cast()` from other scalar values
+#' or extract them with the `$first()` method from a [Series].
+#' ([as_polars_expr()] and [`pl$lit()`][pl__lit] automatically apply the `$first()` method to extract scalar values
+#' inside the default S3 method of [as_polars_expr()].)
 #' @inheritParams as_polars_series
 #' @param as_lit A logical value indicating whether to treat vector as literal values or not.
-#' This argument is always set to `TRUE` when calling this function from `pl$lit()`,
+#' This argument is always set to `TRUE` when calling this function from [`pl$lit()`][pl__lit],
 #' and expects to return literal values. See examples for details.
 #' @return A polars Expression
 #' @seealso
-#' - [as_polars_series()]: R -> Polars type mapping is defined by this function.
+#' - [as_polars_series()]: R -> Polars type mapping is mostly defined by this function.
 #' @examples
 #' # character
-#' as_polars_expr("a")
-#' as_polars_expr(c("a", "b"))
+#' as_polars_expr("a") # Same as `pl$col("a")`
+#' as_polars_expr(c("a", "b")) # Same as `pl$col("a", "b")`
 #' as_polars_expr("a", as_lit = TRUE)
 #' as_polars_expr(c("a", "b"), as_lit = TRUE)
 #'
--- R/functions-lit.R
+++ R/functions-lit.R
@@ -1,3 +1,32 @@
+# TODO: inherit params from cast
+# TODO: link to data type page
+#' Return an expression representing a literal value
+#'
+#' This function is a shorthand for [`as_polars_expr(x, as_lit = TRUE)`][as_polars_expr] and
+#' in most cases, the actual conversion is done by [as_polars_series()].
+#' @inherit as_polars_expr return
+#' @inheritSection as_polars_expr Literal scalar mapping
+#' @param value An R object. Passed as the `x` param of [as_polars_expr()].
+#' @param dtype A polars data type or `NULL` (default).
+#' If not `NULL`, casted to the specified data type.
+#' @seealso
+#' - [as_polars_series()]: R -> Polars type mapping is mostly defined by this function.
+#' - [as_polars_expr()]: Internal implementation of `pl$lit()`.
+#' @examples
+#' # Literal scalar values
+#' pl$lit(1L)
+#' pl$lit(5.5)
+#' pl$lit(NULL)
+#' pl$lit("foo_bar")
+#'
+#' ## Generally, for a vector of length 1, it is converted to a Series and then
+#' ## the `$first()` method is called to become a scalar value.
+#' pl$lit(as.Date("2021-01-20"))
+#' pl$lit(as.POSIXct("2023-03-31 10:30:45"))
+#'
+#' # Literal Series data
+#' pl$lit(1:3)
+#' pl$lit(pl$Series("x", 1:3))
 pl__lit <- function(value, dtype = NULL) {
   if (is.null(dtype)) {
     as_polars_expr(value, as_lit = TRUE)
