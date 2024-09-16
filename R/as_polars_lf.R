4bb2f4d0b317de8f599dc25e9f18c0adf1f0b7cc
eitsupi <ts1s1andn@gmail.com>
2024-09-16 02:09:06 +0000

    feat: `as_polars_lf()` generic function

---
+++ R/as_polars_lf.R
@@ -0,0 +1,29 @@
+# TODO: link to dataframe__lazy
+#' Create a Polars DataFrame from an R object
+#'
+#' The [as_polars_lf()] function creates a [LazyFrame] from various R objects.
+#' It is basically a shortcut for [as_polars_df(x, ...)][as_polars_df] with the
+#' `$lazy()`method.
+#'
+#' ## Default S3 method
+#'
+#' Create a [DataFrame] by calling [as_polars_df()] and then create a [LazyFrame] from the [DataFrame].
+#' Additional arguments `...` are passed to [as_polars_df()].
+#' @inherit pl__LazyFrame return
+#' @inheritParams as_polars_df
+#' @export
+as_polars_lf <- function(x, ...) {
+  UseMethod("as_polars_lf")
+}
+
+#' @rdname as_polars_lf
+#' @export
+as_polars_lf.default <- function(x, ...) {
+  as_polars_df(x, ...)$lazy()
+}
+
+#' @rdname as_polars_lf
+#' @export
+as_polars_lf.polars_lazy_frame <- function(x, ...) {
+  x
+}
