7ed33ff1d19cf6ef448f1b818610349719b4c151
eitsupi <ts1s1andn@gmail.com>
2024-09-16 15:15:13 +0000

    docs: fix typo

--- R/as_polars_lf.R
+++ R/as_polars_lf.R
@@ -1,5 +1,5 @@
 # TODO: link to dataframe__lazy
-#' Create a Polars DataFrame from an R object
+#' Create a Polars LazyFrame from an R object
 #'
 #' The [as_polars_lf()] function creates a [LazyFrame] from various R objects.
 #' It is basically a shortcut for [as_polars_df(x, ...)][as_polars_df] with the
