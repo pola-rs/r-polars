793fc4b467d375534b974dd5885b0c232cd973d5
eitsupi <ts1s1andn@gmail.com>
2024-09-16 13:56:17 +0000

    test: add helper functions to test both lazyframe and dataframe once

---
+++ tests/testthat/helper-expections.R
@@ -0,0 +1,54 @@
+#' Compare the query result with LazyFrame and DataFrame
+#'
+#' Inspired by `compare_dplyr_binding` of the arrow package.
+#' @param object A polars query, must be started with `.input`.
+#' See the examples for details.
+#' @param input R object will be converted to a DataFrame or LazyFrame
+#' by `as_polars_df` or `as_polars_lf`.
+#' @param expected A polars DataFrame, the expected result of the query.
+#' @examples
+#' expect_query_equal(
+#'   .input$select("foo"),
+#'   pl$DataFrame(foo = NULL, bar = NULL),
+#'   pl$DataFrame(foo = NULL)
+#' )
+#' @noRd
+expect_query_equal <- function(object, input, expected) {
+  query <- rlang::enquo(object)
+  out_lazy <- rlang::eval_tidy(query, rlang::new_data_mask(rlang::env(.input = as_polars_lf(input))))$collect()
+  out_eager <- rlang::eval_tidy(query, rlang::new_data_mask(rlang::env(.input = as_polars_df(input))))
+
+  expect_equal(out_lazy, expected)
+  expect_equal(out_eager, expected)
+
+  invisible(NULL)
+}
+
+#' Compare the query error with LazyFrame and DataFrame
+#'
+#' Same as `expect_query_equal()`, but for `expect_error()`.
+#' @param object A polars query, must be started with `.input`.
+#' See `expect_query_equal()` for details.
+#' @param input R object will be converted to a DataFrame or LazyFrame
+#' by `as_polars_df()` or `as_polars_lf()`.
+#' @param regexp passed to `expect_error()`.
+#' @param class passed to `expect_error()`.
+#' @param ... passed to `expect_error()`.
+#' @noRd
+expect_query_error <- function(object, input, regexp = NULL, class = NULL, ...) {
+  query <- rlang::enquo(object)
+  expect_error(
+    rlang::eval_tidy(query, rlang::new_data_mask(rlang::env(.input = as_polars_lf(input))))$collect(),
+    regexp = regexp,
+    class = class,
+    ...
+  )
+  expect_error(
+    rlang::eval_tidy(query, rlang::new_data_mask(rlang::env(.input = as_polars_df(input)))),
+    regexp = regexp,
+    class = class,
+    ...
+  )
+
+  invisible(NULL)
+}
--- tests/testthat/test-lazyframe-frame.R
+++ tests/testthat/test-lazyframe-frame.R
@@ -1,5 +1,29 @@
+test_that("select works lazy/eager", {
+  .data <- pl$DataFrame(
+    int32 = 1:5,
+    int64 = as_polars_series(1:5)$cast(pl$Int64),
+    string = letters[1:5],
+  )
+
+  expect_query_equal(
+    .input$select("int32"),
+    .data,
+    pl$DataFrame(int32 = 1:5)
+  )
+  expect_query_equal(
+    .input$select(pl$lit("int32")),
+    .data,
+    pl$DataFrame(literal = "int32")
+  )
+  expect_query_equal(
+    .input$select(foo = "int32"),
+    .data,
+    pl$DataFrame(foo = 1:5)
+  )
+})
+
 test_that("POLARS_AUTO_STRUCTIFY works for select", {
-  lf <- pl$LazyFrame(
+  .data <- pl$DataFrame(
     foo = 1:3,
     bar = 6:8,
     ham = letters[1:3],
@@ -8,8 +32,9 @@ test_that("POLARS_AUTO_STRUCTIFY works for select", {
   withr::with_envvar(
     c(POLARS_AUTO_STRUCTIFY = "foo"),
     {
-      expect_error(
-        lf$select(1),
+      expect_query_error(
+        .input$select(1),
+        .data,
         r"(Environment variable `POLARS_AUTO_STRUCTIFY` must be one of \('0', '1'\), got 'foo')"
       )
     }
@@ -18,20 +43,18 @@ test_that("POLARS_AUTO_STRUCTIFY works for select", {
   withr::with_envvar(
     c(POLARS_AUTO_STRUCTIFY = "0"),
     {
-      expect_error(
-        lf$select(
-          is_odd = ((pl$col(pl$Int32) %% 2) == 1)$name$suffix("_is_odd"),
-        )$collect(),
+      expect_query_error(
+        .input$select(is_odd = ((pl$col(pl$Int32) %% 2) == 1)$name$suffix("_is_odd")),
+        .data,
         "`keep`, `suffix`, `prefix` should be last expression"
       )
 
-      expect_equal(
+      expect_query_equal(
         withr::with_envvar(c(POLARS_AUTO_STRUCTIFY = "1"), {
-          lf$select(
-            is_odd = ((pl$col(pl$Int32) %% 2) == 1)$name$suffix("_is_odd"),
-          )$collect()
+          .input$select(is_odd = ((pl$col(pl$Int32) %% 2) == 1)$name$suffix("_is_odd"))
         }),
-        lf$select(
+        .data,
+        as_polars_lf(.data)$select(
           is_odd = pl$struct(((pl$col(pl$Int32) %% 2) == 1)$name$suffix("_is_odd")),
         )$collect()
       )
