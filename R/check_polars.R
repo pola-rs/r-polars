b56f9da0967690bac05026347e4b02c55362d511
eitsupi <ts1s1andn@gmail.com>
2024-10-05 04:55:21 +0000

    docs: notes about classes

--- R/check_polars.R
+++ R/check_polars.R
@@ -1,8 +1,16 @@
+# TODO: link to dtype docs
 #' Check if the object is a polars object
 #'
 #' Functions to check if the object is a polars object.
 #' `is_polars_*` functions return `TRUE` of `FALSE` depending on the class of the object.
 #' `check_polars_*` functions throw an informative error if the object is not the correct class.
+#' Prefixes are corresponding to the polars object classes:
+#' - `*_dtype`: For polars data types.
+#' - `*_df`: For [polars data frames][DataFrame].
+#' - `*_expr`: For [polars expressions][Expr].
+#' - `*_lf`: For [polars lazy frames][LazyFrame].
+#' - `*_selector`: For [polars selectors][cs].
+#' - `*_series`: For [polars series][Series].
 #'
 #' `check_polars_*` functions are derived from the `standalone-types-check` functions
 #' from the [rlang][rlang::rlang-package] package
