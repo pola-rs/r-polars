aa2e1ff0df7000c617a11a4a0a808d43a98e2f22
eitsupi <ts1s1andn@gmail.com>
2024-10-06 02:09:30 +0000

    docs: add `pl$show_versions()` page

--- R/meta-versions.R
+++ R/meta-versions.R
@@ -22,6 +22,23 @@
 }
 
 # TODO: what the difference between this and `polars_info()`?
+#' Print out the version of Polars and its optional dependencies
+#'
+#' `r lifecycle::badge("experimental")`
+#' Print out the version of Polars and its optional dependencies.
+#'
+#' These packages may be used for exporting [Series] to R.
+#' See [`<Series>$to_r_vector()`][series__to_r_vector] for details.
+#' - [bit64][bit64::bit64-package]
+#' - [blob][blob::blob]
+#' - [clock][clock::clock-package]
+#' - [data.table][data.table::data.table-package]
+#' - [hms][hms::hms-package]
+#' - [tibble][tibble::tibble-package]
+#' - [vctrs][vctrs::vctrs-package]
+#' @return `NULL` invisibly.
+#' @examples
+#' pl$show_versions()
 pl__show_versions <- function() {
   main_data <- c(
     `Polars:` = .self_version,
@@ -44,4 +61,6 @@ pl__show_versions <- function() {
   cat("\n----Optional dependencies----")
   print(dep_data, right = FALSE)
   cat("\n")
+
+  invisible()
 }
