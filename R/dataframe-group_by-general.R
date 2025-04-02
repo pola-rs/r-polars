90bc98e932455c84c24844c398a289b06b83652d
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2025-04-02 12:50:41 +0200

    docs: Add documentation for GroupBy and LazyGroupBy functions

--- R/dataframe-group_by-general.R
+++ R/dataframe-group_by-general.R
@@ -11,6 +11,29 @@ wrap_to_group_by <- function(x, by, maintain_order) {
   self
 }
 
+#' @inherit lazygroupby__agg title params
+#'
+#' @inherit as_polars_df return
+#' @examples
+#' # Compute the aggregation of the columns for each group.
+#' df <- pl$DataFrame(
+#'   a = c("a", "b", "a", "b", "c"),
+#'   b = c(1, 2, 1, 3, 3),
+#'   c = c(5, 4, 3, 2, 1)
+#' )
+#' df$group_by("a")$agg(pl$col("b"), pl$col("c"))
+#'
+#' # Compute the sum of a column for each group.
+#' df$group_by("a")$agg(pl$col("b")$sum())
+#'
+#' # Compute multiple aggregates at once by passing a list of expressions.
+#' df$group_by("a")$agg(pl$sum("b"), pl$col("c")$mean())
+#'
+#' # Use keyword arguments to easily name your expression inputs.
+#' df$group_by("a")$agg(
+#'   b_sum = pl$sum("b"),
+#'   c_mean_squared = (pl$col("c") ** 2)$mean()
+#' )
 groupby__agg <- function(...) {
   self$df$lazy()$group_by(
     !!!self$by,
@@ -19,6 +42,16 @@ groupby__agg <- function(...) {
     wrap()
 }
 
+#' @inherit lazygroupby__head title params
+#' @inherit as_polars_df return
+#' @examples
+#' df <- pl$DataFrame(
+#'   letters = c("c", "c", "a", "c", "a", "b"),
+#'   nrs = 1:6
+#' )
+#' df
+#'
+#' df$group_by("letters")$head(2)$sort("letters")
 groupby__head <- function(n = 5) {
   self$df$lazy()$group_by(
     !!!self$by,
@@ -27,6 +60,16 @@ groupby__head <- function(n = 5) {
     wrap()
 }
 
+#' @inherit lazygroupby__tail title params
+#' @inherit as_polars_df return
+#' @examples
+#' df <- pl$DataFrame(
+#'   letters = c("c", "c", "a", "c", "a", "b"),
+#'   nrs = 1:6
+#' )
+#' df
+#'
+#' df$group_by("letters")$tail(2)$sort("letters")
 groupby__tail <- function(n = 5) {
   self$df$lazy()$group_by(
     !!!self$by,
--- R/lazyframe-group_by.R
+++ R/lazyframe-group_by.R
@@ -10,17 +10,68 @@ wrap.PlRLazyGroupBy <- function(x, ...) {
   self
 }
 
+#' Compute aggregations for each group of a group by operation
+#'
+#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> Aggregations to compute for
+#' each group of the group by operation. Accepts expression input. Strings are
+#' parsed as column names.
+#'
+#' @inherit as_polars_lf return
+#' @examples
+#' # Compute the aggregation of the columns for each group.
+#' lf <- pl$LazyFrame(
+#'   a = c("a", "b", "a", "b", "c"),
+#'   b = c(1, 2, 1, 3, 3),
+#'   c = c(5, 4, 3, 2, 1)
+#' )
+#' lf$group_by("a")$agg(pl$col("b"), pl$col("c"))$collect()
+#'
+#' # Compute the sum of a column for each group.
+#' lf$group_by("a")$agg(pl$col("b")$sum())$collect()
+#'
+#' # Compute multiple aggregates at once by passing a list of expressions.
+#' lf$group_by("a")$agg(pl$sum("b"), pl$col("c")$mean())$collect()
+#'
+#' # Use keyword arguments to easily name your expression inputs.
+#' lf$group_by("a")$agg(
+#'   b_sum = pl$sum("b"),
+#'   c_mean_squared = (pl$col("c") ** 2)$mean()
+#' )$collect()
 lazygroupby__agg <- function(...) {
   exprs <- parse_into_list_of_expressions(...)
   self$lgb$agg(exprs) |>
     wrap()
 }
 
+#' Get the first `n` rows of each group
+#'
+#' @inheritParams lazyframe__head
+#' @inherit as_polars_lf return
+#' @examples
+#' lf <- pl$LazyFrame(
+#'   letters = c("c", "c", "a", "c", "a", "b"),
+#'   nrs = 1:6
+#' )
+#' lf$collect()
+#'
+#' lf$group_by("letters")$head(2)$sort("letters")$collect()
 lazygroupby__head <- function(n = 5) {
   self$lgb$head(n) |>
     wrap()
 }
 
+#' Get the last `n` rows of each group
+#'
+#' @inheritParams lazyframe__tail
+#' @inherit as_polars_lf return
+#' @examples
+#' lf <- pl$LazyFrame(
+#'   letters = c("c", "c", "a", "c", "a", "b"),
+#'   nrs = 1:6
+#' )
+#' lf$collect()
+#'
+#' lf$group_by("letters")$tail(2)$sort("letters")$collect()
 lazygroupby__tail <- function(n = 5) {
   self$lgb$tail(n) |>
     wrap()
