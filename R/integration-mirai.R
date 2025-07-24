646d9485a6a23264079d186590dd3718bba5c243
eitsupi <ts1s1andn@gmail.com>
2025-07-24 08:24:35 +0000

    fix:  Warn for existing mirai daemons when loaded

--- R/integration-mirai.R
+++ R/integration-mirai.R
@@ -5,6 +5,25 @@ register_mirai_serial <- function() {
   # If the mirai package is not installed, `asNamespace("mirai")` will throw an error,
   # so this function should only be called when the mirai package is loaded.
   if (exists("register_serial", envir = asNamespace("mirai"), mode = "function")) {
+    # If the daemons are already set, registering serialization configs
+    # will not affect existing daemons. So warn to the user.
+    # Safety: mirai::daemons_set is added in mirai 2.3.0,
+    # the same version that introduced the `register_serial` function.
+    if (mirai::daemons_set()) {
+      warn(
+        format_warning(
+          c(
+            `!` = sprintf(
+              "Auto registered %s serialization configs by %s does not affect existing daemons.",
+              format_pkg("mirai"),
+              format_pkg("polars")
+            ),
+            i = "To apply the configs, recreating daemons is needed."
+          )
+        )
+      )
+    }
+
     mirai::register_serial(
       c("polars_data_frame", "polars_lazy_frame", "polars_series"),
       sfunc = list(\(x) x$serialize(), \(x) x$serialize(), \(x) x$serialize()),
