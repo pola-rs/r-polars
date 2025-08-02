c3ecbb96317324cf4911759f82bf51eb00c9ee68
eitsupi <50911393+eitsupi@users.noreply.github.com>
2025-08-02 14:29:39 +0900

    test: Skip the unstable test (#1462)

--- tests/testthat/test-expr-categorical.R
+++ tests/testthat/test-expr-categorical.R
@@ -1,4 +1,7 @@
 test_that("get_categories", {
+  skip("Sinse categories are not stable because global categories are always used now.")
+  # TODO: enable tests after local categorical generation is implemented
+
   dat <- pl$DataFrame(x = factor(c("z", "z", "k", "a", "b")))
   expect_equal(
     dat$select(pl$col("x")$cat$get_categories()),
