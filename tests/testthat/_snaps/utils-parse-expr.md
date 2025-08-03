58503cc54ef122b1ac04236cfacabb5a729a22d9
eitsupi <ts1s1andn@gmail.com>
2025-08-03 09:13:00 +0000

    test: Add tests of the internal function `pase_into_selector`

--- tests/testthat/_snaps/utils-parse-expr.md
+++ tests/testthat/_snaps/utils-parse-expr.md
@@ -26,3 +26,27 @@
       i Element(s) 1, 2, 3 and more are of type list.
       i Perhaps you forgot to use `!!!` on the input(s), e.g. `!!!my_list`?
 
+# parse_into_selector works
+
+    Code
+      parse_into_selector(NULL)
+    Condition
+      Error:
+      ! `...` can only contain single strings or polars selectors.
+
+---
+
+    Code
+      parse_into_selector(integer())
+    Condition
+      Error:
+      ! `...` can only contain single strings or polars selectors.
+
+---
+
+    Code
+      parse_into_selector(pl$lit(NULL))
+    Condition
+      Error:
+      ! `...` can only contain single strings or polars selectors.
+
--- tests/testthat/test-utils-parse-expr.R
+++ tests/testthat/test-utils-parse-expr.R
@@ -20,3 +20,27 @@ test_that("clear error message when passing lists with some Polars expr to dynam
     pl$DataFrame(a = 1:2, b = 3:4, x = vals)
   )
 })
+
+test_that("parse_into_selector works", {
+  # NULL or vectors
+  expect_equal(
+    parse_into_selector(!!!NULL),
+    cs$empty()
+  )
+  expect_snapshot(parse_into_selector(NULL), error = TRUE)
+  expect_equal(
+    parse_into_selector(character()),
+    cs$by_name(require_all = TRUE)
+  )
+  expect_snapshot(parse_into_selector(integer()), error = TRUE)
+
+  # Including selector and expr
+  expect_equal(
+    parse_into_selector(c("foo", "bar"), cs$numeric(), pl$col("baz")),
+    cs$by_name("foo", "bar", require_all = TRUE) |
+      cs$numeric() |
+      cs$by_name("baz", require_all = TRUE)
+  )
+
+  expect_snapshot(parse_into_selector(pl$lit(NULL)), error = TRUE)
+})
