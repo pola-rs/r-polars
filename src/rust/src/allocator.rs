9edb58634d2e14e8f7ea13f01eb655aa6ce2b0ca
eitsupi <50911393+eitsupi@users.noreply.github.com>
2025-09-05 23:42:45 +0900

    chore: Update jemallocator 0.5 to tikv-jemallocator 0.6 (#1529)

--- src/rust/src/allocator.rs
+++ src/rust/src/allocator.rs
@@ -1,15 +1,15 @@
-#[cfg(all(
-    target_family = "unix",
-    not(target_os = "emscripten"),
-    not(allocator = "mimalloc"),
-))]
-use jemallocator::Jemalloc;
 #[cfg(any(
     not(target_family = "unix"),
     target_os = "emscripten",
     allocator = "mimalloc"
 ))]
 use mimalloc::MiMalloc;
+#[cfg(all(
+    target_family = "unix",
+    not(target_os = "emscripten"),
+    not(allocator = "mimalloc"),
+))]
+use tikv_jemallocator::Jemalloc;
 
 #[global_allocator]
 #[cfg(all(
