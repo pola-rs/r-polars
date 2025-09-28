69be07a330590a64d2430828d6d56c5b4d3c5236
eitsupi <ts1s1andn@gmail.com>
2025-09-28 11:02:16 +0000

    docs: fix cross ref

--- R/polars_options.R
+++ R/polars_options.R
@@ -15,7 +15,7 @@
 #'   default value in parenthesis):
 #'
 #' * `compat_level` will affect
-#'    [`as_nanoarrow_array_stream(<series>)`][s3-as_nanoarrow_array_stream]'s
+#'    [`as_nanoarrow_array_stream(<series>)`][as_nanoarrow_array_stream.polars_series]'s
 #'   `polars_compat_level` argument and [`<lazyframe>$sink_ipc()`][lazyframe__sink_ipc]'s
 #'   `compat_level` argument. See the documentation of those functions for details.
 #' * for all `to_r_vector.*` options, see arguments of [to_r_vector()][series__to_r_vector].
