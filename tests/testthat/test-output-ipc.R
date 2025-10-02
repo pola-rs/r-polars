20935b8e0b957474c6c2b7241c188a9656baf5a2
eitsupi <ts1s1andn@gmail.com>
2025-10-02 15:24:13 +0000

    test: add more tests

--- tests/testthat/test-output-ipc.R
+++ tests/testthat/test-output-ipc.R
@@ -78,37 +78,34 @@ test_that("write_ipc: wrong compression", {
 })
 
 patrick::with_parameters_test_that(
-  "Test writing data to Arrow IPC stream",
-  {
-    skip_if_not_installed("nanoarrow")
+  "Test writing data to Arrow stream {rlang::quo_text(compression)} - {compat_level}",
+  .cases = {
+    skip_if_not_installed("arrow")
 
+    expand.grid(
+      compression = list("uncompressed", "zstd", "lz4", NULL),
+      compat_level = list(0, 1, "oldest", "newest")
+    ) |>
+      tibble::as_tibble()
+  },
+  code = {
     df <- pl$DataFrame(
       int = 1:3,
       chr = letters[1:3],
-      cat = factor(letters[1:3])
+      cat = factor(letters[1:3]),
     )
     tmpf <- withr::local_tempfile()
     expect_null(df$write_ipc_stream(tmpf, compression = compression, compat_level = compat_level))
-
-    # Read back and verify
+    expect_snapshot(
+      arrow::read_ipc_stream(tmpf, as_data_frame = FALSE)$schema
+    )
     expect_equal(pl$read_ipc_stream(tmpf), df)
 
-    # Update with new data
-    df$slice(1, 2)$write_ipc_stream(tmpf)
+    # update with new data
+    df$slice(5, 5)$write_ipc_stream(tmpf)
     expect_equal(
       pl$read_ipc_stream(tmpf),
-      df$slice(1, 2)
+      df$slice(5, 5)
     )
-  },
-  compression = list("uncompressed", "zstd", "lz4", NULL),
-  compat_level = list(0, 1, "oldest", "newest")
+  }
 )
-
-test_that("write_ipc_stream: wrong compression", {
-  df <- as_polars_df(iris)
-  tmpf <- withr::local_tempfile()
-  expect_error(
-    df$write_ipc_stream(tmpf, compression = "rar"),
-    "must be one of"
-  )
-})
