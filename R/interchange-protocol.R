04dbb076be3893cedce5e162621d2dc763296caf
eitsupi <ts1s1andn@gmail.com>
2025-10-12 15:52:26 +0000

    refactor: Get CompatLevel's range from the rust side

--- R/interchange-protocol.R
+++ R/interchange-protocol.R
@@ -4,8 +4,8 @@ class(pl__CompatLevel) <- c("polars_object")
 on_load({
   env_bind_lazy(
     pl__CompatLevel,
-    # TODO: call rust function (Single Source of Truth)
-    newest = (function() 1L)(),
-    oldest = (function() 0L)()
+    .range = (function() compat_level_range())(),
+    oldest = (function() pl__CompatLevel$.range[1])(),
+    newest = (function() pl__CompatLevel$.range[2])(),
   )
 })
