d651bda5d6f3429f336dd9f3ae1c4e55496220ae
eitsupi <50911393+eitsupi@users.noreply.github.com>
2025-10-25 19:22:37 +0900

    revert: Revert "chore: Try assigning to avoid strange test error ("This external pointer is already consumed")" (#1602)

--- R/dataframe-s3-waldo.R
+++ R/dataframe-s3-waldo.R
@@ -2,12 +2,12 @@
 
 # exported in zzz.R
 compare_proxy.polars_data_frame <- function(x, path) {
-  list_of_series <- as.list(x, as_series = TRUE)
-  shape <- x$shape
-  schema <- x$schema
-
   list(
-    object = structure(list_of_series, shape = shape, schema = schema),
+    object = structure(
+      as.list(x, as_series = TRUE),
+      shape = x$shape,
+      schema = x$schema
+    ),
     path = path
   )
 }
