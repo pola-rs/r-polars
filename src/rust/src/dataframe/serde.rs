32580888f502b3f187427239c3c329dd2f85a50d
eitsupi <50911393+eitsupi@users.noreply.github.com>
2025-10-31 00:17:00 +0900

    chore: Bump polars to py-1.35.1 (#1615)

--- src/rust/src/dataframe/serde.rs
+++ src/rust/src/dataframe/serde.rs
@@ -1,6 +1,5 @@
 use crate::{PlRDataFrame, RPolarsErr, prelude::*};
 use savvy::{OwnedRawSexp, RawSexp, Result, Sexp, savvy};
-use std::io::BufReader;
 
 #[savvy]
 impl PlRDataFrame {
@@ -10,7 +9,7 @@ impl PlRDataFrame {
     }
 
     fn deserialize_binary(data: RawSexp) -> Result<Self> {
-        let mut reader = BufReader::new(data.as_slice());
+        let mut reader = std::io::Cursor::new(data.as_slice());
         DataFrame::deserialize_from_reader(&mut reader)
             .map(PlRDataFrame::from)
             .map_err(|_| "The input value is not a valid serialized DataFrame.".into())
