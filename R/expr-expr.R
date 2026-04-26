dc39d2e1715788632b659490998752df79aaf168
eitsupi <ts1s1andn@gmail.com>
2026-04-26 07:55:36 +0000

    docs: fix for any

--- R/expr-expr.R
+++ R/expr-expr.R
@@ -1244,7 +1244,11 @@ expr__reshape <- function(dimensions) {
 
 #' Check if any boolean value in a column is true
 #'
-#' @inheritParams expr__all
+#' @inheritParams rlang::args_dots_empty
+#' @param ignore_nulls If `TRUE` (default), ignore null values. If `FALSE`,
+#' [Kleene logic](https://en.wikipedia.org/wiki/Three-valued_logic) is used to
+#' deal with nulls: if the column contains any null values and no `TRUE` values,
+#' the output is null.
 #' @inherit as_polars_expr return
 #' @examples
 #' df <- pl$DataFrame(
