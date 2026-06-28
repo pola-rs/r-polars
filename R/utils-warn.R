cda805820462460c1e079b664456cb1907683d5d
eitsupi <ts1s1andn@gmail.com>
2026-06-28 01:06:24 +0000

    chore: make linter happy

--- R/utils-warn.R
+++ R/utils-warn.R
@@ -3,6 +3,7 @@
 # tooling (e.g. `expect_deprecated()`) continues to match on that class, while
 # `polars_deprecation_warning` and `polars_warning` allow users to catch R-side
 # deprecations with the same handlers used for Rust-side warnings.
+# jarl-ignore duplicated_function_definition: Intended to override the standalone lifecycle's internal warn function.
 .rlang_lifecycle_deprecate_warn0 <- function(
   msg,
   id = msg,
