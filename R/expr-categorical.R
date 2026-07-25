61c7a977f74e6282296b7c1efbfd039ed97860a6
eitsupi <ts1s1andn@gmail.com>
2026-07-25 09:48:35 +0000

    Deprecate <expr>$cat$get_categories()
    Mirrors the upstream Python deprecation (pola-rs/polars#28299): use
    $unique() to get the distinct values in a Categorical column, or
    dtype$categories for the fixed category list of an Enum.

--- R/expr-categorical.R
+++ R/expr-categorical.R
@@ -15,6 +15,13 @@ namespace_expr_cat <- function(x) {
 
 #' Get the categories stored in this data type
 #'
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
+#' `$cat$get_categories()` is deprecated. To get the distinct values present
+#' in a Categorical column, use `$unique()`. For the fixed category list of
+#' an Enum, use its `dtype$categories`.
+#'
 #' @inherit as_polars_expr return
 #' @examples
 #' df <- pl$DataFrame(
@@ -30,6 +37,20 @@ namespace_expr_cat <- function(x) {
 #'   pl$col("vals")$cat$get_categories()
 #' )
 expr_cat_get_categories <- function() {
+  deprecate_warn(
+    c(
+      `!` = sprintf(
+        "%s is deprecated as of %s 1.43.0.",
+        format_fn("cat$get_categories"),
+        format_pkg("polars")
+      ),
+      i = sprintf(
+        "To get the distinct values present in a Categorical column, use %s. For the fixed category list of an Enum, use its %s.", # nolint: line_length_linter
+        format_fn("unique"),
+        format_code("dtype$categories")
+      )
+    )
+  )
   self$`_rexpr`$cat_get_categories() |>
     wrap()
 }
